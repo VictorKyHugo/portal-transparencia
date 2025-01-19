90499a773d7a5d4a3393dc2c825917d354405332
victorkyhugo <kyoouth@gmail.com>
2025-01-19 01:18:48 -0300

    install nuxt icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ import { defineNuxtConfig } from "nuxt/config";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@primevue/nuxt-module", "@nuxtjs/tailwindcss"],
+  modules: ["@primevue/nuxt-module", "@nuxtjs/tailwindcss", "@nuxt/icon"],
   primevue: {
     options: {
       theme: {
