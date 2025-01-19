6c4c79513bfdd9366a8bb94fd421bbd2680e9ee1
victorkyhugo <kyoouth@gmail.com>
2025-01-19 01:18:59 -0300

    create layout footer

---
+++ components/layout/Footer.vue
@@ -0,0 +1,112 @@
+<template>
+  <footer class="text-slate-50 bg-slate-900">
+    <div class="lg:flex justify-center xl:gap-20">
+      <div
+        class="flex justify-self-center flex-col px-10 lg:px-0 lg:flex-row justify-center gap-10"
+      >
+        <div class="p-4">
+          <h1
+            class="border-b-2 mb-4 pb-2 text-center lg:text-start text-xl border-b-yellow-600"
+          >
+            Consultas
+          </h1>
+
+          <div class="grid grid-cols-12">
+            <ul class="col-span-6 space-y-1 mr-2 text-slate-300">
+              <li class="hover:text-white cursor-pointer">Compras</li>
+              <li class="hover:text-white cursor-pointer">Obras</li>
+              <li class="hover:text-white cursor-pointer">Materiais e Bens</li>
+              <li class="hover:text-white cursor-pointer">Receitas</li>
+              <li class="hover:text-white cursor-pointer">Despesas</li>
+              <li class="hover:text-white cursor-pointer">Orçamento</li>
+              <li class="hover:text-white cursor-pointer">Repasses</li>
+              <li class="hover:text-white cursor-pointer">Controladoria</li>
+              <li class="hover:text-white cursor-pointer">Pessoal</li>
+            </ul>
+
+            <ul class="col-span-6 space-y-1 text-slate-300">
+              <li class="hover:text-white cursor-pointer">
+                Conselhos Municipais
+              </li>
+              <li class="hover:text-white cursor-pointer">Informações</li>
+              <li class="hover:text-white cursor-pointer">Saneamento Básico</li>
+              <li class="hover:text-white cursor-pointer">Fundo de Trânsito</li>
+              <li class="hover:text-white cursor-pointer">Integridade</li>
+              <li class="hover:text-white cursor-pointer">Saúde</li>
+              <li class="hover:text-white cursor-pointer">Educação</li>
+              <li class="hover:text-white cursor-pointer">Emergências</li>
+            </ul>
+          </div>
+        </div>
+        <div class="p-4">
+          <h1
+            class="border-b-2 mb-4 text-center lg:text-start pb-2 text-xl border-b-yellow-600"
+          >
+            Institucional
+          </h1>
+
+          <ul class="space-y-1 text-slate-300">
+            <li class="hover:text-white cursor-pointer">LGPD</li>
+            <li class="hover:text-white cursor-pointer">
+              Estrutura Organizacional
+            </li>
+            <li class="hover:text-white cursor-pointer">Secretarias</li>
+            <li class="hover:text-white cursor-pointer">Ouvidoria</li>
+            <li class="hover:text-white cursor-pointer">
+              Biblioteca de Arquivos
+            </li>
+            <li class="hover:text-white cursor-pointer">Imprensa Oficial</li>
+            <li class="hover:text-white cursor-pointer">Pesquisa por Menus</li>
+          </ul>
+        </div>
+        <div class="p-4">
+          <h1
+            class="border-b-2 text-center lg:text-start mb-4 pb-2 text-xl border-b-yellow-600"
+          >
+            Informações
+          </h1>
+
+          <ul class="space-y-1 text-slate-300">
+            <li class="hover:text-white cursor-pointer">
+              Perguntas Frequentes
+            </li>
+            <li class="hover:text-white cursor-pointer">Legislação</li>
+            <li class="hover:text-white cursor-pointer">Glossário</li>
+            <li class="hover:text-white cursor-pointer">Links Úteis</li>
+            <li class="hover:text-white cursor-pointer">Dados abertos</li>
+            <li class="hover:text-white cursor-pointer">Sobre o Portal</li>
+          </ul>
+        </div>
+        <div class="p-4">
+          <h1
+            class="border-b-2 mb-4 pb-2 text-center lg:text-start text-xl border-b-yellow-600"
+          >
+            Controladoria Geral do Município
+          </h1>
+
+          <div class="space-y-1 text-slate-300">
+            <p>Av. Ângelo Giuberti, n° 343, bairro Esplanada Centro</p>
+            <p>Colatina - Espírito Santo</p>
+            <p>CEP - 29.702-060</p>
+            <br />
+            <p>Segunda a Sexta-feira 09h às 18h (presencial ou por contatos)</p>
+            <p class="whitespace-pre">
+              <Icon class="-mb-0.5 mr-1" name="ic:baseline-phone" /> (27)
+              3177-7022
+            </p>
+            <p class="whitespace-pre">
+              <Icon class="-mb-0.5 mr-1" name="ic:baseline-mail-outline" />
+              cgm@colatina.es.gov.br
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <p
+      class="text-slate-200 hover:text-white cursor-pointer px-2 my-4 text-center"
+    >
+      Licença Creative CommonsO conteúdo deste portal está licenciado com uma
+      Licença Creative Commons 4.0 Internacional
+    </p>
+  </footer>
+</template>
