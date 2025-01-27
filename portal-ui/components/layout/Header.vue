577f58045a6e14793fa23536ad1897be4ff0ca9d
victorkyhugo <kyoouth@gmail.com>
2025-01-27 12:11:51 -0300

    fix class label

--- portal-ui/components/layout/Header.vue
+++ portal-ui/components/layout/Header.vue
@@ -2,8 +2,8 @@
   <header class="text-slate-50 bg-zinc-900">
     <div class="ml-4 px-4 py-6">
       <div class="flex flex-col items-start">
-        <h1 className="font-semibold text-2xl ">Portal da Transparência</h1>
-        <div className="text-sm">Prefeitura Municipal de Colatina - ES</div>
+        <h1 class="font-semibold text-xl">Portal da Transparência</h1>
+        <div class="text-sm">Prefeitura Municipal de Colatina - ES</div>
       </div>
     </div>
   </header>
